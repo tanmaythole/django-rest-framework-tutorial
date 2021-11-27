0cfe90b02454ddec52ff108b90446c36bcf92ca8
Tanmay <tanmaythole@gmail.com>
2021-11-27 14:59:20 +0530

    learned about serach filters

--- store/api_views.py
+++ store/api_views.py
@@ -2,18 +2,22 @@ from rest_framework.generics import ListAPIView
 from .serializers import ProductSerializer
 from .models import Product
 from django_filters.rest_framework import DjangoFilterBackend
+# search filters
+from rest_framework.filters import SearchFilter
 
 class ProductList(ListAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    filter_backends = (DjangoFilterBackend, )
+    filter_backends = (DjangoFilterBackend, SearchFilter)
     filter_fields = ('id', )
+    # search fields
+    search_fields = ('name', 'description')
 
     # Knowing about django-filters which helps to search by query like /products/?id=3 shows data of product with id 3
     def get_queryset(self):
         on_sale = self.request.query_params.get('on_sale', None)
         if on_sale == None:
-            return super.get_queryset()
+            return super().get_queryset()
         queryset = Product.objects.all()
         
         if on_sale.lower() == 'true':
